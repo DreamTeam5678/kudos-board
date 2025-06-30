a6406535520dac799b61086704493d3887501166
Pamela Mensah <pmensah@pmensah-ltmy9hq.internal.salesforce.com>
2025-06-30 11:43:53 -0700

    Home and Header components

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Header.css";
+
+const Header = ({searchQuery, setSearchQuery, selectedCategory, setSelectedCategory}) => {
+    const handleSearchChange = (event) => setSearchQuery(event.target.value);
+    const handleCategoryChange = (event) => setSelectedCategory(event.target.value);
+
+
+    return(
+        <header className = "header">
+            <div class name = "logo">
+                Kudos Board
+            </div>
+            <div className = "filters">
+                <select value = {selectedCategory} onChange = {handleCategoryChange}>
+                    <option value = "all">All</option>
+                    <option value = "good">Good</option>
+                    <option value = "bad">Bad</option>
+                    <option value = "ugly">Ugly</option>
+                    <option value = "ok">Ok</option>
+                </select>
+            </div>
+            <div className = "search">
+                <input type = "text"
+                placeholder="Search"
+                value={searchQuery}
+                onChange={handleSearchChange}
+                />
+            </div>
+        </header>
+    );
+}
+
+export default Header;
+
