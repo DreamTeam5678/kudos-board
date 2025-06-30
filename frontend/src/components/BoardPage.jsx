b00a0a0820a62b80d5d8b629fb88b803891e7037
Lauritz Patterson <lauritzpatterson31@yahoo.com>
2025-06-30 12:19:17 -0700

    Add BoardPage component and link it in App.jsx

---
+++ frontend/src/components/BoardPage.jsx
@@ -0,0 +1,14 @@
+// src/components/BoardPage.jsx
+
+import React from "react";
+
+const BoardPage = () => {
+  return (
+    <div>
+      <h1>Board Page</h1>
+      <p>This is where the cards will go.</p>
+    </div>
+  );
+};
+
+export default BoardPage;
