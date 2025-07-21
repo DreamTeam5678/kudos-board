00bfc3bda6b662e0521e2d0281ebc07f351168cd
Pamela Mensah <pmensah@pmensah-ltmy9hq.internal.salesforce.com>
2025-07-21 16:01:04 -0700

    updated for deployment

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,8 @@ const { PrismaClient } = require('@prisma/client');
 
 const app = express();
 const prisma = new PrismaClient();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 // Routes
 const boardsRouter = require('./routes/boards');
